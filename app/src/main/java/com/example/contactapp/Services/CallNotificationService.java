23eaae58ac755f148850bc0aa8682ffaa3b7b6f7
VincentNdegwa <ndegwavincent7@gmail.com>
2024-04-03 23:38:13 +0300

    fixed a small bug

--- app/src/main/java/com/example/contactapp/Services/CallNotificationService.java
+++ app/src/main/java/com/example/contactapp/Services/CallNotificationService.java
@@ -20,37 +20,51 @@ import com.example.contactapp.R;
 
 public class CallNotificationService extends Service {
     private String TAG = "CallNotificationService";
-    private static final int NOTIFICATION_ID= 10;
+    private static final int NOTIFICATION_ID = 10;
     private static final String INCOMING_CHANNEL_ID = "INCOMING_CHANNEL_ID";
     private LocalBroadcastManager localBroadcastManager;
     private static Call mcall;
+    private Notification.Builder ongoingNotificationBuilder;
+
+    private boolean ongoingNotificationCreated = false;
+    private static PendingIntent declinePendingIntent;
+    private static PendingIntent answerPendingIntent;
+    private static PendingIntent contentIntent;
+    private static PendingIntent pendingFullScreenIntent;
+    private static String callerName;
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         localBroadcastManager = LocalBroadcastManager.getInstance(this);
         registerBroadCastReceiver();
-        String callerName = intent.getStringExtra("callerName");
-        PendingIntent declinePendingIntent = intent.getParcelableExtra("declinePendingIntent");
-        PendingIntent answerPendingIntent = intent.getParcelableExtra("answerPendingIntent");
-        PendingIntent contentIntent = intent.getParcelableExtra("contentIntent");
-        PendingIntent pendingFullScreenIntent = intent.getParcelableExtra("pendingFullScreenIntent");
-        if (answerPendingIntent == null && pendingFullScreenIntent == null){
-            Notification notification = buildOngoingNotification(callerName,declinePendingIntent, contentIntent);
-            notification.flags = Notification.FLAG_ONGOING_EVENT;
-            startForeground(NOTIFICATION_ID, notification);
-        }else {
+        callerName = intent.getStringExtra("callerName");
+        declinePendingIntent = intent.getParcelableExtra("declinePendingIntent");
+        answerPendingIntent = intent.getParcelableExtra("answerPendingIntent");
+        contentIntent = intent.getParcelableExtra("contentIntent");
+        pendingFullScreenIntent = intent.getParcelableExtra("pendingFullScreenIntent");
+
+        if (answerPendingIntent == null && pendingFullScreenIntent == null) {
+            if (!ongoingNotificationCreated) {
+                ongoingNotificationBuilder = buildOngoingNotificationBuilder(callerName, declinePendingIntent, contentIntent);
+                Notification notification = ongoingNotificationBuilder.build();
+                notification.flags = Notification.FLAG_ONGOING_EVENT;
+                notification.visibility =Notification.VISIBILITY_PRIVATE;
+                notification.priority =Notification.PRIORITY_LOW;
+                startForeground(NOTIFICATION_ID, notification);
+                ongoingNotificationCreated = true;
+            }
+        } else {
             Notification notification = buildCallNotification(callerName, declinePendingIntent, answerPendingIntent, contentIntent, pendingFullScreenIntent);
             notification.flags = Notification.FLAG_ONGOING_EVENT;
             startForeground(NOTIFICATION_ID, notification);
         }
-
-
         return START_STICKY;
     }
 
-    private Notification buildOngoingNotification(String callerName, PendingIntent declinePendingIntent, PendingIntent contentIntent) {
-        Notification.Builder builder = null;
+    private Notification.Builder buildOngoingNotificationBuilder(String callerName, PendingIntent declinePendingIntent, PendingIntent contentIntent) {
+        Notification.Builder builder;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            builder = new Notification.Builder(this,INCOMING_CHANNEL_ID)
+            builder = new Notification.Builder(this, INCOMING_CHANNEL_ID)
                     .setContentTitle("Outgoing call")
                     .setContentText(callerName)
                     .setContentIntent(contentIntent)
@@ -61,8 +75,7 @@ public class CallNotificationService extends Service {
                             declinePendingIntent
 
                     ));
-        }
-        if (builder == null) {
+        } else {
             builder = new Notification.Builder(this, INCOMING_CHANNEL_ID)
                     .setContentTitle("Outgoing call")
                     .setContentText("callerName")
@@ -71,8 +84,7 @@ public class CallNotificationService extends Service {
                     .setSmallIcon(R.drawable.phone_icon)
                     .setCategory(Notification.CATEGORY_CALL);
         }
-        return  builder.build();
-
+        return builder;
     }
 
     private Notification buildCallNotification(String callerName, PendingIntent declinePendingIntent, PendingIntent answerPendingIntent, PendingIntent contentIntent, PendingIntent pendingFullScreenIntent) {
@@ -86,8 +98,8 @@ public class CallNotificationService extends Service {
                     .setCategory(Notification.CATEGORY_CALL)
                     .setAutoCancel(false)
                     .setOngoing(true)
-                    .setFullScreenIntent(pendingFullScreenIntent,true)
-                    .setFlag(Notification.FLAG_ONGOING_EVENT,true)
+                    .setFullScreenIntent(pendingFullScreenIntent, true)
+                    .setFlag(Notification.FLAG_ONGOING_EVENT, true)
                     .setStyle(Notification.CallStyle.forIncomingCall(
                             new Person.Builder().setName("callerName").build(),
                             declinePendingIntent,
@@ -95,7 +107,6 @@ public class CallNotificationService extends Service {
                     ));
 
         }
-
 
         if (builder == null) {
             builder = new Notification.Builder(this, INCOMING_CHANNEL_ID)
@@ -110,8 +121,6 @@ public class CallNotificationService extends Service {
         return builder.build();
     }
 
-
-
     @Override
     public void onDestroy() {
         super.onDestroy();
@@ -122,22 +131,26 @@ public class CallNotificationService extends Service {
     public IBinder onBind(Intent intent) {
         return null;
     }
+
     private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             int state = intent.getIntExtra("state", -1);
-            switch (state){
+            switch (state) {
                 case Call.STATE_ACTIVE:
-                    stopCalRingTone();
-                    stopForeground(true);
-
+                    if (!ongoingNotificationCreated) {
+                        ongoingNotificationBuilder = buildOngoingNotificationBuilder(callerName,declinePendingIntent , contentIntent);
+                        Notification notification = ongoingNotificationBuilder.build();
+                        notification.flags = Notification.FLAG_ONGOING_EVENT;
+                        startForeground(NOTIFICATION_ID, notification);
+                        ongoingNotificationCreated = true;
+                    }
                     break;
                 case Call.STATE_DISCONNECTED:
-                    stopCalRingTone();
                     stopForeground(true);
                     break;
                 default:
-                    Log.e(TAG, "onReceive: "+ "DEFAULT" );
+                    Log.e(TAG, "onReceive: " + "DEFAULT");
             }
 
         }
